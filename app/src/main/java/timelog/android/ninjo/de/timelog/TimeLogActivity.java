104c0f5d4558863201e93f2cf1fb19d3b35b7b74
renoth <johannes.renoth@gmx.de>
2017-06-14 22:11:04 +0200

    Added Listview for Log Entries (dummy values for now)

--- app/src/main/java/timelog/android/ninjo/de/timelog/TimeLogActivity.java
+++ app/src/main/java/timelog/android/ninjo/de/timelog/TimeLogActivity.java
@@ -8,14 +8,36 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TimeLogActivity extends AppCompatActivity {
+
+    private ListView logList;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_log);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("test");
+        arrayList.add("test 2");
+        arrayList.add("test 3");
+        arrayList.add("test 4");
+        arrayList.add("test 5");
+        arrayList.add("test 6");
+        arrayList.add("test 7");
+
+        logList = (ListView) findViewById(R.id.loglist);
+        adapter = new ArrayAdapter<>(this, R.layout.log_list_content, arrayList);
+
+        logList.setAdapter(adapter);
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
